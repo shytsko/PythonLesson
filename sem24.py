9a351d3b31deae2e3b5359c3888af65cda28259d
Vladimir Shytsko <shytsko.v@gmail.com>
2022-07-27 01:14:18 +0300

    Update

--- sem24.py
+++ sem24.py
@@ -2,71 +2,79 @@
 
 from random import randint
 
-board = [0, 0, 0,
-         0, 0, 0,
-         0, 0, 0]
+EPMTY = 0
+X = 1
+O = -1
+SYMBOLS = {EPMTY: "-", X: "X", O: "O"}
 
-symbols = {0: "-", 1: "X", -1: "O"}
+X_WIN = X
+O_WIN = O
+NOT_END = 0
+DRAW = -2
+
 
 def CheckBoard(brd):
-    winCombinationX = (1, 1, 1)
-    winCombinationO = (-1, -1, -1)
+    winCombinationX = (X, X, X)
+    winCombinationO = (O, O, O)
     combinations = tuple(map(tuple, [brd[0:3], brd[3:6], brd[6:9],
-                                    brd[0:9:3], brd[1:9:3], brd[2:9:3],
-                                    brd[0:9:4], brd[-3:-8:-2]]))
+                                     brd[0:9:3], brd[1:9:3], brd[2:9:3],
+                                     brd[0:9:4], brd[-3:-8:-2]]))
     if winCombinationX in combinations:
-        return 1
+        return X_WIN
     elif winCombinationO in combinations:
-        return -1
-    elif 0 in brd:
-        return 0
+        return O_WIN
+    elif EPMTY in brd:
+        return NOT_END
     else:
-        return -2
+        return DRAW
 
 
 def PrintBoard(brd):
-    print(" ".join(map(lambda i: symbols[i], brd[0:3])))
-    print(" ".join(map(lambda i: symbols[i], brd[3:6])))
-    print(" ".join(map(lambda i: symbols[i], brd[6:9])))
+    print(" ".join(map(lambda i: SYMBOLS[i], brd[0:3])))
+    print(" ".join(map(lambda i: SYMBOLS[i], brd[3:6])))
+    print(" ".join(map(lambda i: SYMBOLS[i], brd[6:9])))
 
 
 def GetMove(brd):
     while True:
         try:
-            m = input("Сделайте ход (строка-столбец): ")
-            i, j = list(map(int, m.split("-")))
+            m = input("Сделайте ход (строка столбец через пробел): ")
+            i, j = list(map(int, m.split()))
             move = (i-1)*3 + (j-1)
-            if brd[move] != 0:
+            if brd[move] != EPMTY:
                 raise
         except:
             print("Неверный ввод")
         else:
             return move
 
+board = [EPMTY, EPMTY, EPMTY,
+         EPMTY, EPMTY, EPMTY,
+         EPMTY, EPMTY, EPMTY]
 
 whoseMove = randint(0, 1)
-if whoseMove == 1:
+if whoseMove != X:
     print("Первым ходит X")
 else:
     print("Первым ходит O")
-end = 0
+end = NOT_END
 PrintBoard(board)
 while not end:
     whoseMove = (whoseMove + 1) % 2
-    if whoseMove == 0:
+    if whoseMove == X:
         print("Ход X")
         move = GetMove(board)
-        board[move] = 1
+        board[move] = X
     else:
         print("Ход O")
         move = GetMove(board)
-        board[move] = -1
+        board[move] = O
     PrintBoard(board)
     end = CheckBoard(board)
 
-if end == 1:
+if end == X_WIN:
     print("Победил X!!!")
-elif end == -1:
+elif end == O_WIN:
     print("Победил O!!!")
 else:
     print("Ничья!!!")
