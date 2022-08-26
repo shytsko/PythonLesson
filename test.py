5e72904f3cbb410afdca99cb57f97f2168f57dd0
Vladimir Shytsko <shytsko.v@gmail.com>
2022-08-26 17:03:11 +0300

    Tast threading

--- test.py
+++ test.py
@@ -1,15 +1,43 @@
-def GetMove(brd):
+from threading import Thread, current_thread
+import random
+from time import sleep
+from queue import Queue
+
+
+def PrintTask(q: Queue):
     while True:
-        try:
-            m = input("Сделайте ход (строка-столбец): ")
-            i, j = list(map(int, m.split("-")))
-            if brd[i][j] != '-':
-                raise
-        except:
-            print("Неверный ввод")
-        else:
-            return (i, j)
+        msg = q.get()
+        print(msg)
+        q.task_done()
 
-board = [['-', 'х', '-'], ['-', '-', '-'], ['-', '-', '-']]
 
-print(GetMove(board))
+def Task(arg, q: Queue):
+    q.put(f"Start thread {current_thread().name}")
+    for i in range(arg):
+        sleep(random.random()*10)
+        q.put(f"Thread {current_thread().name}: {arg=}, {i=}")
+    q.put(f"Stop thread {current_thread().name}")
+
+
+threads = []
+q = Queue()
+
+pr = Thread(target=PrintTask, name="PrintTask", args=(q,))
+pr.setDaemon(True)
+pr.start()
+
+
+threads.append(Thread(target=Task, name="Task1", args=(3, q)))
+threads.append(Thread(target=Task, name="Task2", args=(4, q)))
+threads.append(Thread(target=Task, name="Task3", args=(2, q)))
+threads.append(Thread(target=Task, name="Task4", args=(5, q)))
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
+
+q.put("All task stop")
+
+q.join()
